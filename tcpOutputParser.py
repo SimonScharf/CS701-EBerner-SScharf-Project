a728d838dda52bbc0fcc93f2da7f6337e17cb739
Ethan Berner <eberner@middlebury.edu>
2021-10-07 17:55:21 -0400

    fixed input bug for tcpOutputParser

--- tcpOutputParser.py
+++ tcpOutputParser.py
@@ -1,5 +1,5 @@
 import fileinput 
-for line in fileinput.input(files ='testInput.txt'):
+for line in fileinput.input(files ='tcpOutput.txt'):
     if "length" in line:
         print(line[0:line.index(' ')] + " ", end='')
         print(line[line.index("length") + 6:])
