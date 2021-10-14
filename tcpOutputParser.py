f7d53a8086e6db1f6e8a90bda4b95e44dc6d5dd3
Ethan Berner <eberner@middlebury.edu>
2021-10-14 17:05:43 -0400

    updated parser and fixed formatting issues

--- tcpOutputParser.py
+++ tcpOutputParser.py
@@ -1,6 +1,15 @@
 import fileinput 
 for line in fileinput.input(files ='tcpDumpData.txt'):
     if "length" in line:
-        print(line[0:line.index(' ')] + " ", end='')
-        print(line[line.index("length") + 6:])
-print("we are done")        
+        
+        #get direction, 1 is incoming, -1 is outgoing
+        dir = -1
+        if "> 10.0." in line:
+            dir = 1
+        content = line.split()
+        time = content[0]
+        size = content[-1]
+        graph_plot = int(content[-1])*dir
+
+        print('{:15} {:8} {:2} {:9}'.format(time, size, dir, graph_plot))
+print("we are done")  
