da162c8fa157ec653c8bacb1609ab5ca1f10d6c7
Ethan Berner <eberner@middlebury.edu>
2021-10-19 23:42:06 -0400

    updated functionality of parser, made ip address a field

--- tcpOutputParser.py
+++ tcpOutputParser.py
@@ -5,15 +5,10 @@ for line in fileinput.input(files ='tcpDumpData.txt'):
         #get direction, 1 is incoming, -1 is outgoing
         sourceIP = ""
         destIP = ""
-        dir = -1
-        if "> 10.0." in line:
-            dir = 1
         content = line.split()
         sourceIP = content[2]
         destIP = content[4][:-1]
         time = content[0]
         size = content[-1]
-        graph_plot = int(content[-1])*dir
 
-        print('{:15} s{:20} d{:20} {:8} {:9}'.format(time, sourceIP, destIP, size, graph_plot))
-print("we are done")  
+        print('{:15} s{:20} d{:20} {:8}'.format(time, sourceIP, destIP, size))
