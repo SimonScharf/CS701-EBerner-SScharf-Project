3c66ce9511db00acbafa6eb4404a726811fa482e
Simon Scharf <simonmscharf@gmail.com>
2021-10-21 16:52:08 -0400

    fixed plot.py to accept input file of new format

--- plot.py
+++ plot.py
@@ -12,40 +12,34 @@ for line in fileinput.input(files ='tcpCleanedData.txt'):
         #value is the time, the second is the packet size, and the third is
         #the direction of the packet (incoming or outgoing)
         fields = line.split()
-        print("This is fields: ", end = "") 
-        print(fields)
         if len(fields) > 1 and fields[0] != 'we':
             currTime = fields[0].split(":")[2]
-            print(currTime)
             if firstTimeValue == 0:
                 firstTimeValue = currTime 
-                x.append(currTime)
+                x.append(0)
             else:
-                x.append((float(currTime) - float(firstTimeValue)))
+                x.append(float(currTime) - float(firstTimeValue))
             y.append(float(fields[3]))
 
-print("this is x array and y array:")
 print(x)
 print(y)
 
+#this is how we add lines instead of points
+lines = []
+for i in range(len(x)):
+    pair=[(x[i],0), (x[i], y[i])]
+    lines.append(pair)
 
-#lines = []
-#for i in range(len(x)):
-#    pair=[(x[i],0), (x[i], y[i])]
-#    lines.append(pair)
-
-#linecoll = matcoll.LineCollection(lines)
-#fig, ax = plt.subplots()
-#ax.add_collection(linecoll)
-
-plt.scatter(x,y,marker=".",s=10.0) 
+linecoll = matcoll.LineCollection(lines)
+fig, ax = plt.subplots()
+ax.add_collection(linecoll)
 
 
-plt.axvline(x=0, c="red", label="x=0")
-plt.axhline(y=0, c="yellow", label="y=0")
 
 
-#plt.xticks(x)
-#plt.ylim(0,30)
+#these next two lines add x & y axis
+#plt.axvline(x=0, c="red", label="x=0")
+#plt.axhline(y=0, c="yellow", label="y=0")
 
+plt.scatter(x,y)
 plt.show()
