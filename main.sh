de9b1e88132ffc8c13217b90d483c041e5d7e30d
Simon Scharf <simonmscharf@gmail.com>
2021-10-31 16:51:42 -0400

    made the file output file dynamically named

--- main.sh
+++ main.sh
@@ -12,26 +12,21 @@ fi
 #and already have sudo permissions. (since background tcpdump & cannot read from stdin)
 sudo -v
 
-sudo tcpdump -n > tcpdump.out &
-tcpdump_pid="$!"
-
-echo "tcpdump's pid is $tcpdump_pid"
-
 while read url count; do
     output_filename="$(echo "$url" | awk -F "." '{ print $2 }')"
-
-    curl -o /dev/null https://www.cnn.com
-
-    if test "$?" != "0"; then
-        echo "the curl command failed with: $res"
-    else
-        echo "the curl command ran successfully with: $res"
-    fi
-
     echo "I'm going to fetch $url $count times"
 
     for i in $(seq $count); do
-        echo "interation $i will be saved in ${output_filename}${i}.txt"
+        echo -e "\n\ninteration $i will be saved in ${output_filename}${i}.txt\n"
+    	sudo tcpdump -n > ${output_filename}Data${i}.txt &
+    	tcpdump_pid="$!"
+
+     	curl -o /dev/null $url 
+	    if test "$?" != "0"; then
+            echo "the curl command failed with: $res"
+    	else
+            echo "the curl command ran successfully"
+	    fi
+        kill $tcpdump_pid
     done
-   
 done < $input_file
