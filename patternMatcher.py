3e11a1f45b5ec2e00e73f257aae83e593694087d
Simon Scharf <simonmscharf@gmail.com>
2021-11-01 23:24:10 -0400

    fixed unknownBucketData1 and made patternMatcher take in command line input

--- patternMatcher.py
+++ patternMatcher.py
@@ -3,12 +3,27 @@
 
 #dependencies
 import os
+import sys
+import fileinput
+
+if len(sys.argv) < 2:
+    print("invalid parameters provided to patternMatcher.py")
+    print(str(sys.argv))
+    quit()
+
 
 known = ["CNN", "Reddit"]
 
+unknownFileArr = []
+for line in fileinput.input():
+    unknownFileArr.append(int(line))
+
+
 #unknownFile =  list(map(int,open(os.getcwd() + "/REQUESTS/YouTube/DATA/YouTubeAverageData.txt").read().splitlines()))
-unknownFile = [1, 1, 5, 6, 12, 20, 18, 11, 20, 15, 13, 28, 10, 30, 32, 42, 24, 37, 8, 1]
-print(os.getcwd())
+#unknownFile = [1, 1, 5, 6, 12, 20, 18, 11, 20, 15, 13, 28, 10, 30, 32, 42, 24, 37, 8, 1]
+#print(os.getcwd())
+
+
 CNNData = list(map(int,open(os.getcwd() + "/REQUESTS/CNN/DATA/CNNAverageData.txt").read().splitlines()))
 RedditData = list(map(int,open(os.getcwd() + "/REQUESTS/Reddit/DATA/RedditAverageData.txt").read().splitlines()))
 
@@ -16,9 +31,9 @@ threshold = 50
 CNNMatch = 0
 RedditMatch = 0
 
-for i in range(len(unknownFile)):
-    CNNMatch += abs(unknownFile[i] - CNNData[i])
-    RedditMatch += abs(unknownFile[i] - RedditData[i])
+for i in range(len(unknownFileArr)):
+    CNNMatch += abs(unknownFileArr[i] - CNNData[i])
+    RedditMatch += abs(unknownFileArr[i] - RedditData[i])
 print (RedditMatch, CNNMatch)
 
 if CNNMatch < threshold or RedditMatch < threshold:
